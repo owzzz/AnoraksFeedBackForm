5a92b3784a932bd034b267a4b0cf489c1d8460a9
Owain Llewellyn <owain.llew@gmail.com>
2013-08-07 19:00:36 +0100

    latest

--- Code/app.js
+++ Code/app.js
@@ -10,7 +10,7 @@ app.engine('html', ejs.renderFile);
 app.set('view engine', 'html');
 
 app.configure(function(){
-	console.log();
+	console.log('');
     app.use(express.static(__dirname));
 });
 
@@ -21,12 +21,40 @@ ejs.open = '{{';
 ejs.close = '}}';
 
 var pageData = {
-	title: "AKQA Anoraks"
+	title: "AKQA Anoraks",
+	questions: {
+		questOne: {
+			title: "Overall, how would you rate the event?",
+			answers: {
+				0: {
+					label: "Excellent",
+					type: "radio"
+				},
+				1: {
+					label: "Very Good",
+					type: "radio"
+				},
+				2: {
+					label: "Fairly Good",
+					type: "radio"
+				},
+				3: {
+					label: "Mildly Good",
+					type: "radio"
+				},
+				4: {
+					label: "Not Good at all",
+					type: "radio"
+				}
+			}
+		}
+	}
 }
 
 
 
 app.get('/', function(req, res){
+	console.log('server started on port 3000');
 	res.render('layouts/main', pageData);
 });
 
